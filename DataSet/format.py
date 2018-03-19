86886b26ebab715d3feac37dc246ffda9599e6e1
MarcoMen-Temp <36200629+MarcoMen-Temp@users.noreply.github.com>
2018-03-19 12:53:46 +0000

    Add files via upload

--- DataSet/format.py
+++ DataSet/format.py
@@ -2,7 +2,13 @@
 Adapted from StackOverFlow: 'Formatting Output of CSV file in Python' """ 
 import csv
 
+def concat(*args, sep= " | "):
+      return sep.join(args)
+
+
+print(concat("Petal Length", "Petal Width", "Sepal Length","Sepal Width"))
+
 with open("iris.csv") as f:
       reader = csv.reader(f)
       for row in reader:
-            print('{:<10}  {:<10}  {:<10}  {:<10}' .format(*row))
+            print('{:>12} | {:>11} | {:>12} | {:>11}' .format(*row))
